2d388f70e421a485d4232fe32ff235ee28ce243a
Vladimir <urkytsk3@gmail.com>
2023-04-06 15:01:20 +0800

    Базовый класс MainViewModel

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VkLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Получить экземпляр ViewModel из контейнера зависимостей
+            var viewModel = Ioc.Default.GetService<MainViewModel>();
+
+            // Установить DataContext формы MainWindow в экземпляр ViewModel
+            DataContext = viewModel;
+        }
+    }
+}
